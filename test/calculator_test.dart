aae0d07ac0f5b1c055ae128b93ca28335f1476c7
Yura Movsesyan <useryuramovsesyan@gmail.com>
2023-11-05 15:40:24 +0400

    twopp

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calculator/calculator.dart';
+import 'package:twopp/calculator.dart';
 
 void main() {
   test('adds one to input values', () {
